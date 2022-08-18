72316d08d7d7704ffe39bcf4af32f228a4b97f81
Mario <marandres994@gmail.com>
2022-08-18 11:58:18 -0500

    commit inicial

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		// Se puede reasignar el valor de las variables en java desde que estas no sean constantes
+		int edad = 35;
+		edad = 75;
+		System.out.println(edad); 
+	}
+
+}
